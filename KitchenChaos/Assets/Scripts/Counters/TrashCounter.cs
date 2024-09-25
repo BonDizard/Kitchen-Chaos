b5ac6eddbb44921ee432fef8e540317a036c3b43
BonDizarD <8123bharathbond8123@gmail.com>
2024-09-25 11:34:39 +0530

    trash can logic

--- KitchenChaos/Assets/Scripts/Counters/TrashCounter.cs
+++ KitchenChaos/Assets/Scripts/Counters/TrashCounter.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+/*
+ * Author: Bharath Kumar S
+ * Date: 25-09-2024
+ * Description: Logic for trash can
+ */
 public class TrashCounter : BaseCounter {
     public override void Interact(Player player) {
+        //if player has object then destroy it
         if (player.HasKitchenObject()) {
             player.GetKitchenObject().DestroySelf();
         }
